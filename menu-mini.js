f1f8e6b95b78d49258f7ea4b234012248b670cd3
JenC1109 <jenniclaps.jc@gmail.com>
2022-11-13 15:26:06 -0300

    para entrega final

---
+++ menu-mini.js
@@ -0,0 +1,11 @@
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+function Enviado(event) {
+  alert("Su sugerencia ha sido enviada. Muchas gracias")
+}
